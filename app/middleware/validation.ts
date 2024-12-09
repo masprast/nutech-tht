092b826711ceb2b67d7d9e183a0d43cf3c335ccb
masprast <henry.prasetya.kurniawan@gmail.com>
2024-12-09 11:46:34 +0700

    fixed route

--- app/middleware/validation.ts
+++ app/middleware/validation.ts
@@ -7,6 +7,7 @@ const validationMiddleware = (schema: AnyZodObject) => async (req: Request, _: R
 		await schema.parseAsync({ body: req.body, query: req.query, params: req.params });
 		return next();
 	} catch (error) {
+		_.json(req.body);
 		if (error instanceof ZodError) {
 			return next(new herror.BadRequest());
 		}
