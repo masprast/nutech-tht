ad28ac0c22031ff042912260089817d6f0e93e72
masprast <henry.prasetya.kurniawan@gmail.com>
2024-12-09 15:31:22 +0700

    fixing something

--- app/app.ts
+++ app/app.ts
@@ -1,38 +1,58 @@
-import express, { Request, Response } from "express";
+// import express, { Request, Response } from "express";
+// import helmet from "helmet";
+// import { env } from "./config/env";
+// import pool from "./config/db";
+// import router from "./routes/route";
+
+// class App {
+// 	private app: express.Application;
+// 	constructor() {
+// 		this.app = express();
+// 		this.initMiddleware();
+// 		this.initController();
+// 	}
+
+// 	private initMiddleware() {
+// 		this.app.use(helmet());
+// 	}
+
+// 	private initController() {
+// 		this.app.use("/", router);
+// 		this.app.get("/", (req: Request, res: Response) => {
+// 			res.send("server running");
+// 		});
+// 	}
+
+// 	listen() {
+// 		this.app.listen(env.PORT, () => {
+// 			console.log("=== LOG ===");
+
+// 			pool.on("connect", () => {
+// 				console.log("server running");
+// 			});
+// 			console.log(`App run on port ${env.PORT}`);
+// 		});
+// 	}
+// }
+
+// export default App;
+
+import express, { Response } from "express";
 import helmet from "helmet";
+import route from "./routes/route";
 import { env } from "./config/env";
-import pool from "./config/db";
-import router from "./routes/route";
 
-class App {
-	private app: express.Application;
-	constructor() {
-		this.app = express();
-		this.initMiddleware();
-		this.initController();
-	}
+const app = express();
 
-	private initMiddleware() {
-		this.app.use(helmet());
-	}
+app.use(helmet());
+app.use(express.json());
 
-	private initController() {
-		this.app.use("/", router);
-		this.app.get("/", (req: Request, res: Response) => {
-			res.send("server running");
-		});
-	}
+app.use("/", route);
+app.get("/", (res: Response) => {
+	res.send("server is runnung");
+});
 
-	listen() {
-		this.app.listen(env.PORT, () => {
-			console.log("=== LOG ===");
-
-			pool.on("connect", () => {
-				console.log("server running");
-			});
-			console.log(`App run on port ${env.PORT}`);
-		});
-	}
-}
-
-export default App;
+app.listen(env.PORT, () => {
+	console.log(`app is running on ${env.PORT}`);
+});
+export default app;
