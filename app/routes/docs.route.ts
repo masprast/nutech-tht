f266f8df4a7ce945e6ce118b75b4f10e6a24d78b
masprast <henry.prasetya.kurniawan@gmail.com>
2024-12-09 18:31:08 +0700

    fixing swagger

--- app/routes/docs.route.ts
+++ app/routes/docs.route.ts
@@ -3,10 +3,10 @@ import swaggerUI from "swagger-ui-express";
 import swaggerJSDoc from "swagger-jsdoc";
 import doc from "../utils/swagger";
 
-const swaggerDef = swaggerJSDoc({ swaggerDefinition: doc, apis: ["./app/routes/*.route.ts"] });
+const swaggerDef = swaggerJSDoc({ swaggerDefinition: doc, apis: ["app/routes/*.route.ts"] });
 
 const router = Router();
-router.use("/docs", swaggerUI.serve);
+router.use("/", swaggerUI.serve);
 router.get("/docs", swaggerUI.setup(swaggerDef, { explorer: true }));
 
 export default router;
